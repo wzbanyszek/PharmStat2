2ea15a1cf6ee8b15fa574096afa481cd9ae439e1
wzbanyszek <wzbanyszek.santo@gmail.com>
2025-02-01 22:06:08 +0500

    Update Wprowadzenie.py

--- pages/Wprowadzenie.py
+++ pages/Wprowadzenie.py
@@ -1,39 +1,34 @@
 import streamlit as st
 from utils.translations import translations
 
-
 def show(language):
     t = translations[language]
 
     st.header(t["intro"])
     st.write(f"""
-    **{t["intro"]}**  
-    Aplikacja umożliwia przeprowadzanie analizy danych statystycznych i jakościowych w prosty i intuicyjny sposób. W bocznym menu znajdziesz moduły, które pomogą Ci w analizie danych z różnych perspektyw.
+    {t["intro_desc"]}
 
     **{t["menu_title"]}:**
 
-    1. **{t["intro"]}**  
-       {t["intro_desc"]}
-
-    2. **{t["descriptive_stats"]}**  
+    1. **{t["descriptive_stats"]}**  
        {t["descriptive_stats_desc"]}
 
-    3. **{t["histograms"]}**  
+    2. **{t["histograms"]}**  
        {t["histograms_desc"]}
 
-    4. **{t["boxplot"]}**  
+    3. **{t["boxplot"]}**  
        {t["boxplot_desc"]}
 
-    5. **{t["control_charts"]}**  
+    4. **{t["control_charts"]}**  
        {t["control_charts_desc"]}
 
-    6. **{t["process_capability"]}**  
+    5. **{t["process_capability"]}**  
        {t["process_capability_desc"]}
 
-    7. **{t["stability_regression"]}**  
+    6. **{t["stability_regression"]}**  
        {t["stability_regression_desc"]}
 
-    8. **{t["temp_humidity"]}**  
+    7. **{t["temp_humidity"]}**  
        {t["temp_humidity_desc"]}
 
     ---
