941fae803ff072d666c4f4d2bdb176968ee7ed50
wzbanyszek <wzbanyszek.santo@gmail.com>
2025-02-01 14:06:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,4 @@ import streamlit as st
 # Konfiguracja strony
 st.set_page_config(page_title="Santo Pharmstat", layout="wide")
 
-# Menu boczne
-st.sidebar.title("Menu")
-page = st.sidebar.radio(
-    "Wybierz podstronę:",
-    [
-        "Wprowadzenie",
-        "Wykres punktowy i liniowy",
-        "Regresja liniowa (SciPy)",
-        "Wczytanie pliku Excel",
-        "Analiza temperatury i wilgotności",
-        "Karta kontrolna Shewharta (X-bar / R)"
-    ]
-)
-
+st.title("Santo Pharmstat")
