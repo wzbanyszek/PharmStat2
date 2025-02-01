989177076d50766a91778be484ee6f9a8d5d8547
wzbanyszek <wzbanyszek.santo@gmail.com>
2025-02-01 21:06:09 +0500

    Create translations.py

---
+++ utils/translations.py
@@ -0,0 +1,38 @@
+translations = {
+    "Polski": {
+        "menu_title": "Menu",
+        "intro": "Witaj w aplikacji Santo Pharmstat!",
+        "choose_page": "Wybierz podstronę:",
+        "descriptive_stats": "Statystyki opisowe",
+        "histograms": "Histogramy",
+        "boxplot": "Wykresy pudełkowe BoxPlot",
+        "control_charts": "Karty kontrolne ImR",
+        "process_capability": "Analiza zdolności procesowej",
+        "stability_regression": "Regresja dla stabilności",
+        "temp_humidity": "Analiza temperatury i wilgotności"
+    },
+    "English": {
+        "menu_title": "Menu",
+        "intro": "Welcome to Santo Pharmstat!",
+        "choose_page": "Select a page:",
+        "descriptive_stats": "Descriptive Statistics",
+        "histograms": "Histograms",
+        "boxplot": "BoxPlot Charts",
+        "control_charts": "ImR Control Charts",
+        "process_capability": "Process Capability Analysis",
+        "stability_regression": "Stability Regression",
+        "temp_humidity": "Temperature and Humidity Analysis"
+    },
+    "Русский": {
+        "menu_title": "Меню",
+        "intro": "Добро пожаловать в Santo Pharmstat!",
+        "choose_page": "Выберите страницу:",
+        "descriptive_stats": "Описательная статистика",
+        "histograms": "Гистограммы",
+        "boxplot": "Диаграммы BoxPlot",
+        "control_charts": "Контрольные карты ImR",
+        "process_capability": "Анализ способности процесса",
+        "stability_regression": "Регрессия для стабильности",
+        "temp_humidity": "Анализ температуры и влажности"
+    }
+}
