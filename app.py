5f3611153d20ac6039968aae3b2463c06403ca39
wzbanyszek <wzbanyszek.santo@gmail.com>
2025-02-01 21:11:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,51 +11,54 @@ language = st.sidebar.selectbox(
     index=0  # Ustawienie domyślnego języka (0 = Polski, 1 = English, 2 = Русский)
 )
 
+# Ustawienie tłumaczeń na podstawie wybranego języka
+t = translations[language]
+
 # Menu boczne
-st.sidebar.title("Menu")
+st.sidebar.title(t["menu_title"])
 page = st.sidebar.radio(
-    "Wybierz podstronę:",
+    t["choose_page"],
     [
-        "Wprowadzenie",
-        "Statystyki opisowe",
-        "Histogramy",
-        "Wykresy pudełkowe BoxPlot",
-        "Karty kontrolne ImR",
-        "Analiza zdolności procesowej",
-        "Regresja dla stabilności",
-        "Analiza temperatury i wilgotności"
+        t["intro"],
+        t["descriptive_stats"],
+        t["histograms"],
+        t["boxplot"],
+        t["control_charts"],
+        t["process_capability"],
+        t["stability_regression"],
+        t["temp_humidity"]
     ]
 )
 
 # Routing podstron
-if page == "Wprowadzenie":
+if page == t["intro"]:
     from pages import Wprowadzenie
-    Wprowadzenie.show()
+    Wprowadzenie.show(language)
 
-elif page == "Statystyki opisowe":
+elif page == t["descriptive_stats"]:
     from pages import descriptive_statistics
-    descriptive_statistics.show()
+    descriptive_statistics.show(language)
 
-elif page == "Histogramy":
+elif page == t["histograms"]:
     from pages import histogram_analysis
-    histogram_analysis.show()
+    histogram_analysis.show(language)
 
-elif page == "Wykresy pudełkowe BoxPlot":
+elif page == t["boxplot"]:
     from pages import BoxPlot
-    BoxPlot.show()
+    BoxPlot.show(language)
 
-elif page == "Karty kontrolne ImR":
+elif page == t["control_charts"]:
     from pages import control_charts
-    control_charts.show()
+    control_charts.show(language)
 
-elif page == "Analiza zdolności procesowej":
+elif page == t["process_capability"]:
     from pages import process_capability
-    process_capability.show()
+    process_capability.show(language)
 
-elif page == "Regresja dla stabilności":
+elif page == t["stability_regression"]:
     from pages import stability_analysis
-    stability_analysis.show()
+    stability_analysis.show(language)
 
-elif page == "Analiza temperatury i wilgotności":
+elif page == t["temp_humidity"]:
     from pages import Analiza_temperatury_wilgotnosci
-    Analiza_temperatury_wilgotnosci.show()
+    Analiza_temperatury_wilgotnosci.show(language)
