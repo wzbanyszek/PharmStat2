b0a7c0b62ebd54377c3a6642581103c6eb2bb32c
wzbanyszek <wzbanyszek.santo@gmail.com>
2025-02-06 13:25:09 +0500

    Update pqr.py

--- AppPages/pqr.py
+++ AppPages/pqr.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
 from SPC import ImRControlChart, Rule01, Rule02, Rule03, Rule04, Rule05, Rule06, Rule07, Rule08
+from utils.translations import translations
 
 def show(language):
     t = translations[language]["pqr_module"]
