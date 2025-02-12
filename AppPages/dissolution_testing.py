b00f67fc2c9f2c66e946b820f8ed964f9bace58a
Wojciech Zbanyszek <wzbanyszek.santo@gmail.com>
2025-02-12 17:54:05 +0500

    Update dissolution_testing.py

--- AppPages/dissolution_testing.py
+++ AppPages/dissolution_testing.py
@@ -12,7 +12,8 @@ def calculate_f2(reference, test):
     """
     n = len(reference)
     sum_sq_diff = np.sum((reference - test) ** 2)
-    f2 = 50 * np.log10(1 + (1/n) * sum_sq_diff) * (-0.5)
+    #f2 = 50 * np.log10(1 + (1/n) * sum_sq_diff) * (-0.5)
+    f2 = 50 * np.log10(100*((1 + (1/n) * sum_sq_diff) ** (-0.5)))
     f2 = abs(f2)
     return f2
 
