7b03080c0bc6c58c85f3eeebca1c80f991d11975
Wojciech Zbanyszek <wzbanyszek.santo@gmail.com>
2025-03-06 17:27:09 +0500

    Update test.py

--- AppPages/test.py
+++ AppPages/test.py
@@ -15,9 +15,8 @@ import xlsxwriter
 # Funkcja do konwersji DataFrame do pliku Excel
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    writer.save()
+    with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+        df.to_excel(writer, index=False, sheet_name="Sheet1")
     processed_data = output.getvalue()
     return processed_data
 
